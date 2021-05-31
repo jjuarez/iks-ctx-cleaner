83c27909f5e32949daeb749a0d40cd2ca7ddc6aa
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-06-01 01:12:46 +0200

    root command test placeholders

---
+++ cmd/root_test.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadFile(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(false, "put your serious tests here")
+}
+
+func TestUnmarshalYAML(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(false, "put your serious tests here")
+}
+
+func TestCleanupYAML(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(false, "put your serious tests here")
+}
+
+func TestMarshalYAML(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(false, "put your serious tests here")
+}
