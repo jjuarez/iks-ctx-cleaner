6a2d4654a6c8391829d91796f3ad9a8d8166a633
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-05-31 11:30:40 +0200

    Isolated data structure for better control over the marshal process

--- cmd/root.go
+++ cmd/root.go
@@ -22,9 +22,11 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -32,45 +34,90 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type KubeConfig struct {
-	ApiVersion  string   `yaml:"apiVersion"`
-	Kind        string   `yaml:"kind"`
-	Preferences struct{} `yaml:"preferences,omitempty"`
-	Clusters    []struct {
-		Cluster struct {
-			CertificateAuthorityData string `yaml:"certificate-authority-data"`
-			Server                   string `yaml:"server"`
-		}
-		Name string `yaml:"name"`
-	}
-	Contexts []struct {
-		Context struct {
-			Cluster   string `yaml:"cluster"`
-			Namespace string `yaml:"namespace"`
-			User      string `yaml:"user"`
-		}
-		Name string `yaml:"name"`
-	}
-	CurrentContext string `yaml:"current-context"`
+/*
+ * Cluster configuration structure
+ */
+type Cluster struct {
+	CertificateAuthorityData string `yaml:"certificate-authority-data,omitempty"`
+	Server                   string `yaml:"server,omitempty"`
 }
 
-func dumpData(kubeConfig *KubeConfig) {
-	log.Printf("Cluster.name: %s", kubeConfig.Clusters[0].Name)
-	log.Printf("Cluster.certificate-authority-data: %s", kubeConfig.Clusters[0].Cluster.CertificateAuthorityData)
-	log.Printf("Cluster.server: %s", kubeConfig.Clusters[0].Cluster.Server)
-	log.Printf("Context.name: %s", kubeConfig.Contexts[0].Name)
-	log.Printf("Context.cluster: %s", kubeConfig.Contexts[0].Context.Cluster)
-	log.Printf("Context.namespace: %s", kubeConfig.Contexts[0].Context.Namespace)
-	log.Printf("Context.user: %s", kubeConfig.Contexts[0].Context.User)
+type Clusters []struct {
+	Cluster Cluster `yaml:"cluster,omitempty"`
+	Name    string  `yaml:"name,omitempty"`
+}
+
+/*
+ * Context configuration structure
+ */
+type Context struct {
+	Cluster   string `yaml:"cluster,omitempty"`
+	Namespace string `yaml:"namespace,omitempty"`
+	User      string `yaml:"user,omitempty"`
+}
+
+type Contexts []struct {
+	Context Context `yaml:"context,omitempty"`
+	Name    string  `yaml:"name,omitempty"`
+}
+
+/*
+ * Users configuration structure
+ */
+type UsersConfig struct {
+	ClientId     string `yaml:"client-id,omitempty"`
+	ClientSecret string `yaml:"client-secret,omitempty"`
+	IdToken      string `yaml:"id-token,omitempty"`
+	IdpIssueURL  string `yaml:"idp-issuer-url,omitempty"`
+	RefreshToken string `yaml:"refresh-token,omitempty"`
+}
+
+type UsersAuthProvider struct {
+	Config UsersConfig `yaml:"config,omitempty"`
+	Name   string      `yaml:"name,omitempty"`
+}
+
+type User struct {
+	AuthProvider UsersAuthProvider `yaml:"auth-provider,omitempty"`
+}
+
+type Users []struct {
+	Name string `yaml:"name,omitempty"`
+	User User   `yaml:"user,omitempty"`
+}
+type KubeConfig struct {
+	ApiVersion     string   `yaml:"apiVersion,omitempty"`
+	Kind           string   `yaml:"kind,omitempty"`
+	Preferences    struct{} `yaml:"preferences,omitempty"`
+	Clusters       Clusters `yaml:"clusters,omitempty"`
+	Contexts       Contexts `yaml:"contexts,omitempty"`
+	CurrentContext string   `yaml:"current-context,omitempty"`
+	Users          Users    `yaml:"users,omitempty"`
+}
+
+func cleanContextName(kubeConfig *KubeConfig) {
+	contextIdIndex := strings.Index(kubeConfig.Contexts[0].Name, "/")
+
+	if contextIdIndex != -1 {
+		kubeConfig.Contexts[0].Name = kubeConfig.Contexts[0].Name[0:contextIdIndex]
+	}
+}
+
+func cleanCurrentContext(kubeConfig *KubeConfig) {
+	contextIdIndex := strings.Index(kubeConfig.CurrentContext, "/")
+
+	if contextIdIndex != -1 {
+		kubeConfig.CurrentContext = kubeConfig.CurrentContext[0:contextIdIndex]
+	}
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "iksctxcleaner",
+	Use:   "cat your_iks_kubeconfig.yaml|ikscc",
 	Short: "IKS context cleaner",
-	Long: `Small utility to clean the IBMCloud k8s context names to make easier to use them with a toolchain for humans
+	Long: `Small utility to clean the IBMCloud IKS context names to make easier to use them with a toolchain for humans
 examples and usage of using your application. For example:
 
-cat $HOME/.kube/config | iksctxcleaner
+cat $HOME/.kube/config | ikscc
 
 `,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -81,7 +128,15 @@ cat $HOME/.kube/config | iksctxcleaner
 		if err != nil {
 			log.Fatalf("error: %v", err)
 		} else {
-			dumpData(kubeConfig)
+			cleanContextName(kubeConfig)
+			cleanCurrentContext(kubeConfig)
+
+			yamlOutput, err := yaml.Marshal(&kubeConfig)
+			if err != nil {
+				log.Fatalf("Ops! There was an error: %v", err)
+			} else {
+				fmt.Print(string(yamlOutput))
+			}
 		}
 	},
 }
