e356e67218db79a2122add90bd0278f870aa6f72
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2021-06-01 16:50:49 +0200

    Tests skaffold

---
+++ model/kubeconfig_test.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFoo(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(true, "put your serious tests here")
+}
