57bc7a08b96b8155589e33c390daca1efae6ad11
JJ <javier.juarez.martinez@ibm.com>
2024-07-25 10:43:12 +0200

    chore: A test place holder

---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadFile(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(true, "put your serious tests here")
+}
